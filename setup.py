f5e12dfd349708d3ffbf4f3b5790f58fd9912597
Paul <deprazz@gmail.com>
2017-05-31 16:29:08 -0300

    setup tools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='commons',
+    version='0.6',
+    packages=['', 'pt_br'],
+    url='https://github.com/PaulDepraz/commons',
+    license='GPLv3',
+    author='Paul Depraz',
+    author_email='deprazz@gmail.com',
+    description='This is the chant of a poor man.'
+)
