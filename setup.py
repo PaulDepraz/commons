44406f431a73a9c04b6a460638f2fd461b6d243a
PaulDepraz <deprazz@gmail.com>
2017-06-23 12:17:07 -0300

    de Jesus

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='commons',
     version='0.8.1',
-    packages=['commons', 'commons.helper', 'commons.mvc', 'commons.pt_br', 'commons.rest'],
+    packages=['commons', 'commons.helper', 'commons.mvc', 'commons.mvc.generic', 'commons.pt_br', 'commons.rest'],
     url='https://github.com/PaulDepraz/commons',
     license='GPLv3',
     author='Paul Depraz',
